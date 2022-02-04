52024d28800dd5357c78da2b7a657d7172279ad0
Tyler887 <tylermageeshields@gmail.com>
2022-02-04 20:05:22 +0000

    Create check.bat

---
+++ check.bat
@@ -0,0 +1,5 @@
+@echo off
+echo "SAFESYS VIRUS CHECKER V1.0"
+echo "Licensed under the Apache License, version 2.0,"
+echo "you may not use this utility except in accordance with the license."
+echo "You can obtain a copy at https://apache.org/licenses/LICENSE-2.0."
